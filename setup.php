115d79c0ce3aae329deb43f8df8e11335247315f
WilliamAckerman <148882361+WilliamAckerman@users.noreply.github.com>
2024-05-18 12:36:19 -0400

    first commit

---
+++ setup.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$con = new mysqli($servername, $username, $password);
+if($con->connect_error){
+    die("Connection failed: " . $con->connect_error);
+}
+
+$sql = "CREATE DATABASE IF NOT EXISTS CARTED";
+if($con->query($sql) === TRUE){
+    echo "Database created successfully";
+}
+else {
+    echo "Error creating database: " . $con->error;
+}
+
+$con->close();
+?>
---
+++ setupTables.php
@@ -0,0 +1,47 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'CARTED';
+
+$con = new mysqli($servername, $username, $password, $db);
+if($con->connect_error){
+    die("Connection failed: " . $con->connect_error);
+}
+
+$sql = "DROP TABLE IF EXISTS CARTELS";
+if($con->query($sql) === TRUE) {
+    echo "Cartels table being initialized...";
+}
+$sql2 = "CREATE TABLE CARTELS (
+    cartelID INT PRIMARY KEY AUTO_INCREMENT,
+    cartelName VARCHAR(50) NOT NULL UNIQUE,
+    stateAbbr CHAR(2) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    country VARCHAR(30) NOT NULL
+    )";
+if($con->query($sql2) === TRUE) {
+    echo "Cartels table initialized.";
+}
+else {
+    echo "Failed to initialize cartels table: " . $con->error;
+}
+
+$sql3 = "DROP TABLE IF EXISTS CRIMES";
+if($con->query($sql3) === TRUE) {
+    echo "Crimes table being initialized...";
+}
+$sql4 = "CREATE TABLE CRIMES (
+    crimeID INT PRIMARY KEY AUTO_INCREMENT,
+    crimeName VARCHAR(24) UNIQUE,
+    crimeDesc VARCHAR(100) NOT NULL
+    )";
+if($con->query($sql4) === TRUE) {
+    echo "Crimes table initialized."
+}
+else {
+    echo "Failed to initialized crimes table: " . $con->error;
+}
+
+$con->close();
+?>
