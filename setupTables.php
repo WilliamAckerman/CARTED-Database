4c67063a45de726ebb5809167fc3398833880110
WilliamAckerman <148882361+WilliamAckerman@users.noreply.github.com>
2024-05-18 12:50:32 -0400

    Misc. Commit

--- setupTables.php
+++ setupTables.php
@@ -43,5 +43,10 @@ else {
     echo "Failed to initialized crimes table: " . $con->error;
 }
 
+$sql5 = "DROP TABLE IF EXISTS DRUGS";
+if($con->query($sql3) === TRUE) {
+    echo "Drugs table being initialized...";
+}
+
 $con->close();
 ?>
