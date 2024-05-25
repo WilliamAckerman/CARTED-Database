e60c2cb32398d9166ef64dab875837b9ec9b9a54
WilliamAckerman <148882361+WilliamAckerman@users.noreply.github.com>
2024-05-25 13:35:53 -0400

    Started working on the search process for the DRUGS table.

--- about.php
+++ about.php
@@ -5,15 +5,27 @@ session_start();
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="header.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <title>About</title>
 <link rel="icon" type="image/x-icon" href="LogoImage.png">
 </head>
 <body class="body">
-<div class="w3-container w3-teal">
-        <h1>About</h1>
+<div class="header">
+    <a href="LogoImage.png" class="logo">About Us</a>
+    <div class="header-right">
+        <a class="active" href="mainPage.php">Home</a>
+        <a href="#contact">Contact Us</a>
+        <a href="about.php">About Us</a>
     </div>
-    <br><img src="LogoImage.png" alt="Logo Image" class="center"><br><br>
+</div>
+    <br><img src="LogoImage.png" alt="Logo Image" class="center">
+    <div class="divText">
+        <h2>Our Mission</h2>
+    <p>
+        [Description goes here.]
+    </p>
+    </div>
     <form action="mainPage.php">
         <div class="divText">
         <button class = "button" value="Submit">Return</button>
